6012c8935d63943aab7d4911793865335f5234d0
Niko <nikomedvesky@gmail.com>
2024-10-15 16:18:06 -0500

    Added custom class mode

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,6 +3,9 @@ extends Node2D
 func wait(seconds):
     await get_tree().create_timer(seconds).timeout
 
+func _ready():
+    update_custom_menu()
+
 func _on_andy_timer_timeout():
     if randf() <= 0.6:
         %AndyAnim.frame = (randi() % 2) + 1
@@ -31,6 +34,8 @@ func _on_new_game_pressed():
     Students.sean.init(root)
     Students.john.init(root)
     SceneSwitcher.switch_scene("res://scenes/office.tscn")
+
+
 
 ### Hover selection arrows >>
 func add_selection_arrows(button):
@@ -64,3 +69,74 @@ func _on_settings_mouse_entered():
 
 func _on_settings_mouse_exited():
     remove_selection_arrows(%settings)
+
+
+
+
+
+var andy_level = 20
+var sean_level = 20
+var john_level = 20
+
+func update_custom_menu():
+    %AndyLevel.text = str(andy_level)
+    %SeanLevel.text = str(sean_level)
+    %JohnLevel.text = str(john_level)
+
+func _on_increase_andy_pressed():
+    andy_level += 1
+    andy_level = clamp(andy_level, 0, 20)
+    update_custom_menu()
+
+func _on_decrease_andy_pressed():
+    andy_level -= 1
+    andy_level = clamp(andy_level, 0, 20)
+    update_custom_menu()
+
+func _on_increase_sean_pressed():
+    sean_level += 1
+    sean_level = clamp(sean_level, 0, 20)
+    update_custom_menu()
+
+func _on_decrease_sean_pressed():
+    sean_level -= 1
+    sean_level = clamp(sean_level, 0, 20)
+    update_custom_menu()
+
+func _on_increase_john_pressed():
+    john_level += 1
+    john_level = clamp(john_level, 0, 20)
+    update_custom_menu()
+
+func _on_decrease_john_pressed():
+    john_level -= 1
+    john_level = clamp(john_level, 0, 20)
+    update_custom_menu()
+
+
+func _on_custom_game_pressed():
+    %AndyAnim.visible = false
+    %Title.visible = false
+    %v_buttons.visible = false
+
+    %CustomGameMenu.visible = true
+
+func _on_back_button_custom_pressed():
+    %AndyAnim.visible = true
+    %Title.visible = true
+    %v_buttons.visible = true
+
+    %CustomGameMenu.visible = false
+
+
+func _on_start_button_custom_pressed():
+    Students.andy.update_level(andy_level)
+    Students.sean.update_level(sean_level)
+    Students.john.update_level(john_level)
+
+    var root = get_tree().get_root()
+    Students.andy.init(root)
+    Students.sean.init(root)
+    Students.john.init(root)
+    SceneSwitcher.switch_scene("res://scenes/office.tscn")
+
